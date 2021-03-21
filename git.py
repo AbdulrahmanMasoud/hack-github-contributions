efed36d82720ce67258a466800ebd5c2e183d5a7
unknown <shaksia13@gmail.com>
2021-03-21 07:50:03 +0200

    Hack Git Contributions

---
+++ git.py
@@ -0,0 +1,14 @@
+import os , random
+
+for i in range(200):
+    d = str(i) + 'days ago'
+    rand = random.randrange(1, 12)
+    with open('test.txt','a') as file:
+        file.write(d+'\n')
+    os.system('git add test.txt')
+    os.system('git commit --date=" 2019-'+str(rand)+'-'+d+'" -m 1')
+os.system('git push -u origin main')
+
+#git commit --amend --no-edit --date="Fri Nov 6 20:00:00 2015 -0600" 
+#git fetch origin master
+#git rebase origin/master
