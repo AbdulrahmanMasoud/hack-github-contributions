dfe361c9d29d6cd4f6d5a07e67c2500a1ca2d084
sbatrow <79014783+sbatrow@users.noreply.github.com>
2021-06-09 21:01:41 +0530

    Update git.py

--- git.py
+++ git.py
@@ -6,7 +6,7 @@ for i in range(200):
     with open('test.txt','a') as file:
         file.write(d+'\n')
     os.system('git add test.txt')
-    os.system('git commit --date=" 2019-'+str(rand)+'-'+d+'" -m 1')
+    os.system('git commit --date=" 2020-'+str(rand)+'-'+d+'" -m 1')
 os.system('git push -u origin main')
 
 #git commit --amend --no-edit --date="Fri Nov 6 20:00:00 2015 -0600" 
